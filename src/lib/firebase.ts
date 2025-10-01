fabdb13d7bcb9069c007366e014ceee9277838ae
khadija bilal <khadijabilal888@gmail.com>
2025-10-01 09:29:33 +0000

    same error check Auth setings

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOchAhYDrFnISXJT3aNfL-dAnRq1qXL3M",
-  authDomain: "studio-4185006276-c9a8a.firebaseapp.com",
-  projectId: "studio-4185006276-c9a8a",
-  storageBucket: "studio-4185006276-c9a8a.appspot.com",
-  messagingSenderId: "818248571629",
-  appId: "1:818248571629:web:e00b85d0ae4031ee578ae4"
+    apiKey: "AIzaSyCOchAhYDrFnISXJT3aNfL-dAnRq1qXL3M",
+    authDomain: "studio-4185006276-c9a8a.firebaseapp.com",
+    projectId: "studio-4185006276-c9a8a",
+    storageBucket: "studio-4185006276-c9a8a.appspot.com",
+    messagingSenderId: "818248571629",
+    appId: "1:818248571629:web:e00b85d0ae4031ee578ae4",
 };
 
+// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
